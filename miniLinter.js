f116da6239834183083815fa99dbc421d14a604e
Pete <peterwalsh86@hotmail.com>
2017-12-29 17:05:00 +0000

    Create miniLinter.js
    In this project, you will use what you know about iterating over arrays to improve the quality of a paragraph and gather some information about that paragraph.
    
    This is the same type of work that word processing software does. Additionally, you may have heard of linting, a process by which text is evaluated and improved by an application. In this project, you will create a miniature version of a linter using array methods that you have learned.

---
+++ miniLinter.js
@@ -0,0 +1,55 @@
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+// split the story string into individual words
+let storyWords = story.split(" ");
+
+// log how many words are in the story to the console
+console.log("There are " + storyWords.length + " words in this story.");
+
+// Iterate over your array to filter out unncessary words. 
+let betterWords = storyWords.filter(function(word){
+  if(unnecessaryWords.includes(word) === false) {
+    return word;
+  }
+});
+console.log(betterWords);
+
+// variables for counting how many times an overused word is used
+let overused0 = 0;
+let overused1 = 0;
+let overused2 = 0;
+// variable for counting the number of sentences
+let sentences = 0;
+
+for(let word in betterWords) {
+  if(word = overusedWords[0]) {
+     overused0 ++;
+  } else if(word = overusedWords[1]) {
+     overused1 ++;
+  } else if(word = overusedWords[2]) {
+     overused2 ++;
+  }
+}
+
+for(let word in storyWords) {
+  console.log("THe word is " + storyWords[word]);
+  for(let letter of word){
+    console.log("The letter is " + word[letter]);
+    if(letter === "." || letter === "!"){
+     sentences++;
+    }
+  }
+}
+
+console.log("The word count is " + storyWords.length + ".");
+console.log("The sentence count is " + sentences + ".");
+console.log("'" + overusedWords[0] + "' is used " + overused0 + " times.");
+console.log("'" + overusedWords[1] + "' is used " + overused1 + " times.");
+console.log("'" + overusedWords[2] + "' is used " + overused2 + " times.");
+
+console.log(betterWords.join(" "));
+

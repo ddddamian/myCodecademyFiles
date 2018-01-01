d2d9f347c20e4ffaebefeb4d2d50b438791ab4a2
Pete <peterwalsh86@hotmail.com>
2018-01-01 23:19:03 +0000

    Update teamStats.js
    Wrote code to solve this problem.

--- teamStats.js
+++ teamStats.js
@@ -9,3 +9,92 @@ Once created, you can manipulate these data structures as well as gain insights 
 For example, you might want to get the total number of games your team has played, or the average score of all of their games.
 
 */
+
+// object representing a team
+let team = {
+  // array of the players on the team
+  _players: [
+    {
+      firstName: "Kenneth",
+      lastName: "Zohore",
+      age: 23
+    },
+    {
+      firstName: "Joe",
+      lastName: "Ralls",
+      age: 24
+    },
+    {
+      firstName: "Junior",
+      lastName: "Hoilett",
+      age: 27
+    }
+  ],
+  
+  // array of the games the team plays
+  _games: [
+    {
+      opponent: "Fulham",
+      teamPoints: 2,
+      opponentPoints: 4
+    },
+    {
+      opponent: "Preston",
+      teamPoints: 0,
+      opponentPoints: 1
+    },
+    {
+      opponent: "QPR",
+      teamPoints: 1,
+      opponentPoints: 2
+    }
+  ],
+  
+  // getter method for players
+  get players() {
+    return this._players;
+  },
+  
+  // getter method for games
+  get games() {
+    return this._games;
+  },
+  
+  // method to add a player to the team
+  addPlayer(firstName, lastName, age) {
+    let player = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    }
+    // adds the new player to the _players array
+    this._players.push(player);
+  },
+  
+  // method to add a game
+  addGame(opponentName, teamPoints, opponentPoints) {
+    let game = {
+      opponent: opponentName,
+      teamPoints: teamPoints,
+      opponentPoints: opponentPoints
+    }
+    // adds the new game to the _games array
+    this._games.push(game);
+  }
+};
+
+// add 3 new players to the team
+team.addPlayer("Steph", "Curry", 28);
+team.addPlayer("Lisa", "Leslie", 44);
+team.addPlayer("Bugs", "Bunny", 76);
+
+// log all players
+console.log(team.players);
+
+// add 3 new games to the team
+team.addGame("Mansfield", 2, 0);
+team.addGame("Sunderland", 3, 1);
+team.addGame("Sheffield Wed", 2, 2);
+
+// log all games
+console.log(team.games);
